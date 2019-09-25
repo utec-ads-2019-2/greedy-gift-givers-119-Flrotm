499bd72f5b6a9c4f626ab537f6e64507b3d6addd
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-09-25 17:23:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+
+int main() {
+int personas=0,amount=0,regalos=0;
+string receptor;
+map<string,int> dineros;
+
+while(cin>>personas) {
+    string nombres[personas];
+    for (int i = 0; i < personas; i++) {
+        cin>>nombres[i];
+        dineros[nombres[i]]=0;
+    }
+
+    for(int i =0;i<personas;i++){
+        string dador;
+        cin>>dador>>amount>>regalos;
+
+        for(int j=1;j<=regalos;j++){
+            cin>>receptor;
+            dineros[receptor]+=amount/regalos;
+          }
+            if (regalos!=0)
+                dineros[dador]-=(amount/regalos*regalos);
+       }
+    for (int i = 1;i<=personas; i++){
+        cout << nombres[i-1] << " " << dineros[nombres[i-1]] << endl;
+    if(i==personas)
+        cout<<endl;}
+    }
+}
+
+
+
 
